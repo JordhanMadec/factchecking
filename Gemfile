f5419c29236d3247da92b6dd6a08b50c749e10f1
JordhanMadec <jordhan.madec@gmail.com>
2017-03-06 22:37:09 +0100

    Essai migration db vers mysql + nettoyage. Installer gem stemmify + mysql

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'mysql'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -27,6 +27,7 @@ gem 'twitter', '5.16.0'
 
 gem 'bootstrap-sass', '~> 3.3.7'
 
+gem 'stemmify'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 require 'twitter'
+require 'stemmify'
 
 class PagesController < ApplicationController
   def init
@@ -10,18 +11,40 @@ class PagesController < ApplicationController
     end
   end
 
-  def home
-
-  end
-
   def search_tweets
     client = init
-    #@twittos = client.user(params[:keywords])
-    #@tweet_list = client.user_timeline(params[:keywords],).take(100)
     @keywords = params[:keywords] ||= "test"
     #Si la liste de mots-clés est vide, Twitter API renvoie une erreur
     if @keywords=="" then @keywords = "test" end
-    @tweet_list = client.search(@keywords, lang: "en")
-    @nbTweets = @tweet_list.count
+    #Nettoyage des tweets
+    @tweet_list =  clean_tweets client.search(@keywords, lang: "en")
+    @nbTweets = @tweet_list.count #Nombre de tweets trouvés
+  end
+
+  def clean_tweets(tweets)
+      res = Hash.new
+      tweets.each do |tweet|
+        #Les tweets retournés par l'API sont en mode frozen, ils ne sont pas modifiables
+        #Il faut donc récupérer une copie (.dup) de chaque attribut pour pouboir les modifier
+        res[tweet.id] = Hash.new
+        res[tweet.id]["favorite_count"] = tweet.favorite_count.to_s
+        res[tweet.id]["retweet_count"] = tweet.retweet_count
+        res[tweet.id]["source"] = tweet.source.dup
+        res[tweet.id]["attrs"] = tweet.attrs.dup
+        res[tweet.id]["user"] = tweet.user.dup
+        res[tweet.id]["in_reply_to_id"] = tweet.in_reply_to_user_id
+        res[tweet.id]["text"] = tweet.text.dup
+        #1.Downcase  2.Rootify  3.Delete useless terms
+        res[tweet.id]["cleaned_text"] = rootify tweet.text.dup.downcase
+      end
+      res
+  end
+
+  def rootify(tweet)
+    token  = tweet.split(" ")
+    token.each do |term|
+      term = term.stem
+    end
+    token.join(" ")
   end
 end
