f19b304bfea316aca51d72bbaf6f9b6e6f0c049e
JordhanMadec <jordhan.madec@gmail.com>
2017-03-23 22:40:57 +0100

    Ajout d'un dataset (dataset.json) et des fonctions correspondantes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 require 'twitter'
 require 'stemmer'
-require'sentimental' #à rajouter dans le gemfile
+require'sentimental'
 require 'config_dev'
+require 'json'
 
 if ConfigDev.PB_SSL
   require 'openssl'
@@ -10,6 +11,8 @@ end
 
 class PagesController < ApplicationController
 
+  THRESHOLD = 0.5
+    
   def init
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = ConfigDev.CONSUMER_KEY
@@ -25,13 +28,17 @@ class PagesController < ApplicationController
     #Si la liste de mots-clés est vide, Twitter API renvoie une erreur
     if @keywords=="" then @keywords = "test" end
     #Nettoyage des tweets
-    @tweet_list =  clean_tweets client.search(@keywords, lang: "en")
+    #@tweet_list =  prepare_tweets client.search(@keywords, lang: "en")
+    #clean_tweets @tweet_list
+    @tweet_list = JSON.parse(get_dataset)
     @nbTweets = @tweet_list.count #Nombre de tweets trouvés
+    #Analyse sentimentale des tweets
     @tweet_list = sentimental_and_score_analysis @tweet_list
-
+    #Classification des tweets
+    #set_dataset(@tweet_list)
   end
 
-  def clean_tweets(tweets)
+  def prepare_tweets(tweets)
       res = Hash.new
       tweets.each do |tweet|
         #Les tweets retournés par l'API sont en mode frozen, ils ne sont pas modifiables
@@ -44,36 +51,50 @@ class PagesController < ApplicationController
         res[tweet.id]["user"] = tweet.user.dup
         res[tweet.id]["in_reply_to_id"] = tweet.in_reply_to_user_id
         res[tweet.id]["text"] = tweet.text.dup
+        res[tweet.id]["cleaned_text"] = ""
         res[tweet.id]["sentimental_class"] = "default"
         res[tweet.id]["sentimental_score"] = 0
-        #1.Downcase  2.Rootify  3.Delete useless terms
-        res[tweet.id]["cleaned_text"] = stemmify tweet.text.dup.downcase
       end
-      res
+      res.to_json
+  end
+      
+  def clean_tweets(tweets)
+     tweets.each do |key, tweet|
+        #1.Downcase  2.Rootify  (3.Delete useless terms)
+        tweet["cleaned_text"] = stemmify tweet["text"].downcase
+     end
+  end
+      
+  #Fonction créant un dataset avec le résultat de la requête
+  def set_dataset(tweets)
+    File.open("dataset.json", "w") do |f|
+        f.write(tweets)
+    end 
+  end
+      
+  def get_dataset()
+      file = File.read("dataset.json")
   end
 
   def stemmify(tweet)
-    token  = tweet.split(" ")
+    token = tweet.split(" ")
     token.map! do |term|
       term.stem
     end
     token.join(" ")
   end
 
-
-  $THRESHOLD = 0.5
-
   def sentimental_class(text)
     analyzer = Sentimental.new
     analyzer.load_defaults
-    analyzer.threshold = $THRESHOLD
+    analyzer.threshold = THRESHOLD
     analyzer.sentiment text
   end
 
   def sentimental_score(text)
     analyzer = Sentimental.new
     analyzer.load_defaults
-    analyzer.threshold = $THRESHOLD
+    analyzer.threshold = THRESHOLD
     analyzer.score text
   end
 
